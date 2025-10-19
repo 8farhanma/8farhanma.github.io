c652480b5a9e46dd07b95354f39d7af150c28480
Farhan MAulana <8farhanmaulana@gmail.com>
2025-10-19 17:11:40 +0700

    update intro

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -78,7 +78,7 @@ const TYPING_SPEED = 100;
 const ERASING_SPEED = 50;
 const PAUSE_DURATION = 2000;
 const WORDS = ["Fresh Graduate of Information Systems","Certified BNSP (Software Engineer)","Full Stack Web Developer","System Analyst","Freelance","Tech Enthusiast"];
-const TECH_STACK = ["PHP","Laravel", "CodeIgniter", "Python", "MySQL", "UML Tool"];
+const TECH_STACK = ["PHP","Laravel", "CodeIgniter", "MySQL", "UML Tool"];
 const SOCIAL_LINKS = [
   { icon: Github, link: "https://github.com/8farhanma" },
   { icon: Linkedin, link: "https://www.linkedin.com/in/8farhanma" },
