6f07d83cfbaf33bb63e6f119e1e6a58d92f9ce92
Farhan MAulana <8farhanmaulana@gmail.com>
2025-10-19 17:32:48 +0700

    update about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -241,7 +241,7 @@ const AboutPage = () => {
         </div>
         
         <blockquote className="text-gray-300 text-center lg:text-left italic font-medium text-sm relative z-10 pl-6">
-          “It’s not about how sophisticated the application is, but how useful it is to its users.”
+          “You can’t compete with someone who's genuinely having fun.”
         </blockquote>
       </div>
 
